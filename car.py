52d094bd38da4af8e469e6541063b8746bcd689c
qchuchu <quentin.churet@outlook.fr>
2019-09-25 09:10:05 +0200

    Modifying Virtual Env

---
+++ car.py
@@ -0,0 +1,6 @@
+class Car:
+    """Cette classe retournera un objet itinéraire qui donnera des informations
+    sur un trajet en voiture entre deux positions GPS
+    """
+
+    def __init__(self, departure)
---
+++ helpers.py
@@ -0,0 +1,5 @@
+# Ce fichier contient des fonctions communes aux différentes classes,
+# facilitant la lisibilité du code
+
+def url_here_routing_api():
+    base = 'https://route.api.here.com/routing/7.2/'
