c002266ba3325bec4f35f54aba7a18ece68e642f
qchuchu <quentin.churet@outlook.fr>
2019-09-25 13:57:20 +0200

    Test

--- helpers.py
+++ helpers.py
@@ -1,5 +1,9 @@
 # Ce fichier contient des fonctions communes aux différentes classes,
 # facilitant la lisibilité du code
 
+import os
+# Pour accéder aux APP_ID et aux APP_CODE, il faut utiliser l'os.environ
+
 def url_here_routing_api():
     base = 'https://route.api.here.com/routing/7.2/'
+
