0f0f5c3669beae1dad06fd0f62749987456f3453
qchuchu <quentin.churet@outlook.fr>
2019-10-08 16:56:32 +0200

    Creating Get Itinerary Scooter

--- gpsapp/helpers.py
+++ gpsapp/helpers.py
@@ -5,8 +5,9 @@ import os
 # Pour accéder aux APP_ID et aux APP_CODE, il faut utiliser l'os.environ
 import requests
 # Pour faire des requêtes get
+from gpsapp.views import app
 
-def url_here_routing_api(waypoint0, waypoint1):
+def url_here_routing_api(origin, destination):
     """
     Cette fonction retourne l'URL nécessaire pour faire des requêtes GET à l'API Here
     :param waypoint0: Coordonnées GPS de départ, sous forme de tuples de string
@@ -15,18 +16,18 @@ def url_here_routing_api(waypoint0, waypoint1):
     """
 
     base = 'https://route.api.here.com/routing/7.2/calculateroute.json'
-    app_id = "?app_id=" + os.environ['APP_ID']
-    app_code = "&app_code=" + os.environ['APP_CODE']
-    waypoint0 = "&waypoint0=geo!" + ",".join(waypoint0)
-    waypoint1 = "&waypoint1=geo!" + ",".join(waypoint1)
+    app_id = "?app_id=" + app.config['APP_ID']
+    app_code = "&app_code=" + app.config['APP_CODE']
+    origin = "&waypoint0=geo!" + ",".join(origin)
+    destination = "&waypoint1=geo!" + ",".join(destination)
     mode = "&mode=fastest;car;traffic:disabled"
     language = "&language=fr-fr"
-    return base + app_id + app_code + waypoint0 + waypoint1 + mode + language
+    return base + app_id + app_code + origin + destination + mode + language
 
 def url_here_weather_api():
     base = 'https://weather.api.here.com/weather/1.0/report.xml'
-    app_id = "?app_id=" + os.environ['APP_ID']
-    app_code = "&app_code=" + os.environ['APP_CODE']
+    app_id = "?app_id=" + app.config['APP_ID']
+    app_code = "&app_code=" + app.config['APP_CODE']
     product = "&product=observation"
     name = "&name=Paris"
     return base + app_id + app_code + product + name
