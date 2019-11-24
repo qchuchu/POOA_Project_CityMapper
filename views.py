e2b58f66926f782b7caa78a5a957ef3b16cb25ed
qchuchu <quentin.churet@outlook.fr>
2019-11-24 21:15:39 +0100

    Changing API Call

--- views.py
+++ views.py
@@ -4,22 +4,14 @@ from flask import request
 from flask_cors import CORS
 from transportation_api.itinerary_optimizer import ItineraryOptimizer
 
-ITINERARIES = []
-
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-@app.route('/itineraries', methods=['GET', 'POST'])
+@app.route('/itineraries', methods=['POST'])
 def get_best_itinerary():
-    global ITINERARIES
     response_object = {'status': 'success'}
-    if request.method == 'POST':
-        del ITINERARIES[:]
-        data = request.get_json()
-        optimizer = ItineraryOptimizer(data)
-        optimizer.run()
-        response_object['itineraries'] = optimizer.get_itineraries_json()
-        ITINERARIES = response_object['itineraries']
-    elif request.method == 'GET':
-        response_object['itineraries'] = ITINERARIES
+    data = request.get_json()
+    optimizer = ItineraryOptimizer(data)
+    optimizer.run()
+    response_object['itineraries'] = optimizer.get_itineraries_json()
     return response_object
